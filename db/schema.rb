f09ce4dd4ce02cf59500f86e0aa14bda945584ad
mattward0028 <mattward0028@gmail.com>
2017-01-17 14:05:11 -0800

    migration to change body column type to text

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170117082713) do
+ActiveRecord::Schema.define(version: 20170117215822) do
 
   create_table "posts", force: :cascade do |t|
     t.string   "title"
-    t.string   "body"
+    t.text     "body"
     t.string   "author"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
