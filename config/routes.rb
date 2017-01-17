c7f0dbd29d80cf8378f0ac8e9911e80a9f0d4c96
mattward0028 <mattward0028@gmail.com>
2017-01-16 21:05:46 -0800

    created scaffold for posts / made posts index root

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
+  resources :posts
   get 'static_pages/index'
 
-  root 'static_pages#index'
+  root 'posts#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
