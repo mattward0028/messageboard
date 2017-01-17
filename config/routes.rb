40dd15ce8ae62210b4482a61dff0954e8f126a23
mattward0028 <mattward0028@gmail.com>
2017-01-17 15:02:27 -0800

    created model comment

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :users
-  resources :posts
+  resources :posts do
+  resources :comments
+end
   get 'static_pages/index'
 
   root 'posts#index'
