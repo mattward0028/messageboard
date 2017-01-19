cda54ed1fd7b0c68bdaba8187ff277c427f363d6
mattward0028 <mattward0028@gmail.com>
2017-01-18 21:30:44 -0800

    removed notice

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
-
-describe User do
+ 
+ describe User do 
   context "when user is not valid" do
      it "check the user validation" do
        expect(User.new(password: "123")).not_to be_valid
